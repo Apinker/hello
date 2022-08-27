2ec5c98f3185caa7359543d846cd031906d26650
Apinker <apinkerman2@huskers.unl.edu>
2022-08-27 18:59:11 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+    printf("Hello,I'm Aiden Pinkerman/n");
+    printf("Major is Computer Engineer");
+
+    return 0;
+}
