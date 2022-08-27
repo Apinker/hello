920170700cc1b5070361ec8741278fd1ea60dd8e
Apinker <apinkerman2@huskers.unl.edu>
2022-08-27 20:20:12 +0000

    added minor and hobby

--- hello.c
+++ hello.c
@@ -7,7 +7,9 @@
 int main(int argc, char **argv) {
 
     printf("Hello,I'm Aiden Pinkerman/n");
-    printf("Major is Computer Engineer");
+    printf("Major is Computer Engineer/n");
+    printf("I don't have a minor/n")
+    printf("Hobby, messing around on the computer)
 
     return 0;
 }
