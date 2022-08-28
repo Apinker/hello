bdfc71d5e0a66c581543644c6815214121b118bf
Alaa Ismail <aismail8@huskers.unl.edu>
2022-08-28 15:49:23 +0000

    added my name and major too

--- hello.c
+++ hello.c
@@ -6,10 +6,11 @@
 
 int main(int argc, char **argv) {
 
-    printf("Hello,I'm Aiden Pinkerman/n");
-    printf("Major is Computer Engineer/n");
-    printf("I don't have a minor/n")
-    printf("Hobby, messing around on the computer)
+    printf("Hello, I'm Aiden Pinkerman.\n");
+    printf("Hello, I'm Alaa Ismail. My major is Global Studies and Computer Science.\n");
+    printf("Major is Computer Engineer.\n");
+    printf("I don't have a minor.\n");
+    printf("Hobby, messing around on the computer.\n");
 
     return 0;
 }
